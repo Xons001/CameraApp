e2e61b9c17c35f05870d318cd29baabf2f242e29
Sean <sean.saez.f@gmail.com>
2018-11-26 19:34:09 +0100

    Imagen guardada

--- app/src/main/java/com/camera1/camera1/MainActivity.java
+++ app/src/main/java/com/camera1/camera1/MainActivity.java
@@ -2,6 +2,9 @@ package com.camera1.camera1;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,10 +14,18 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mCurrentPhotoPath;
+    static ImageView nImageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.layout_main);
 
         Button b = findViewById(R.id.button);
+
 
         b.setOnClickListener(new View.OnClickListener() {
              @Override
@@ -34,6 +46,12 @@ public class MainActivity extends AppCompatActivity {
          }
 
         );
+        nImageView = findViewById(R.id.foto);
+        File file = new File("/data/data/com.camera1.camera1/files/a.jpg");
+        Uri uir = Uri.fromFile(file);
+        nImageView.setImageURI(uir);
+
+
     }
 
     @Override
@@ -41,19 +59,36 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ImageView nImageView = findViewById(R.id.foto);
+            nImageView = findViewById(R.id.foto);
             nImageView.setImageBitmap(imageBitmap);
 
             // guardar a disc
-             /*try{
-                FileOutputStream out = new FileOutputStream(IMAGE_FILENAME: MODE_APPEND)
-                imageBitmap.compress(Bitmap.CompressFormat.JPEG, quality: 100, out);
-                Log.v(tag: "TAG OK", msg: "Arxiu ok")
+             try{
+                FileOutputStream out =  openFileOutput("a.jpg", MODE_APPEND);
+                imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+                out.close();
+                Log.v( "TAG OK", "Arxiu ok");
             }catch (Exception e){
-                Log.v(tag: "FILE ERROR", msg: "Error escrivint arxiu");
+                Log.v( "FILE ERROR", "Error escrivint arxiu");
                 e.printStackTrace();
-            }*/
-
+            }
         }
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
